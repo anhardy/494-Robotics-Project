2d6209b41b8ba559a68e15431b042e1083989cab
Adam Hardy <ahardy313@gmail.com>
2021-03-31 19:36:11 -0700

    Initial setup of flocking

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))] //Needed to see what's around the feesh
+public class Flock : MonoBehaviour
+{
+    Collider feeshCollider;
+    public Collider FeeshCollider {
+        get {
+            return feeshCollider;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        feeshCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FlockingBehavior.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FlockingBehavior : ScriptableObject
+{
+    public abstract Vector3 CalcDirection (Feesh feesh,  List<Transform> context, Flock flock); //Context list is list of neighbors around feesh.
+}
