0f1b84da460a78c05e58e53d5f379df7fd433906
Adam Hardy <ahardy313@gmail.com>
2021-04-14 12:40:01 -0700

    Free assets. Camera fog. Minor behavior changes.

---
+++ Assets/Scripts/WaterFog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = new Color (0.22f, 0.65f, 0.77f, 0.5f);
+        RenderSettings.fogDensity = 0.025f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
